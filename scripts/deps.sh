7867fe0f425758d8222074c50ada4187d55065bb
Tim Collins <tim@thecollins.team>
2023-08-14 12:50:37 +0100

    fix: get renovate to play nice with kustomize

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -16,7 +16,7 @@ echo Using architecture ${ARCH}
 curl -SL https://get.helm.sh/helm-${HELM_VERSION}-linux-${ARCH}.tar.gz | tar -xz linux-${ARCH}/helm && mv linux-${ARCH}/helm /usr/local/bin/
 
 # Install Kustomize
-curl -SL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_${ARCH}.tar.gz | tar -xzC /usr/local/bin
+curl -SL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F$(echo ${KUSTOMIZE_VERSION}|tr -d kustomize/)/kustomize_$(echo ${KUSTOMIZE_VERSION}|tr -d kustomize/)_linux_${ARCH}.tar.gz | tar -xzC /usr/local/bin
 
 # Install yq
 curl -L -s "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_${ARCH}" -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
